921e69b8127d3bad456931fec296c20c67a10329
MAJU STUDENT <majustudent@jinnah.edu>
2019-04-28 18:40:39 +0500

    Code Updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,70 @@
-//App.js in src dir is using the Login Form and Header to set up the main UI for index.js at root for the App
 import React, { Component } from "react";
 import { View } from "react-native";
 // import the firebase third party lib
 import firebase from "firebase";
 // Custom Components to be used in the app
-//import { Header } from "./components/common/Header";
-
-import { Header } from "./common/";
+import {
+Header,
+CustomButton,
+CardSection,
+Card,
+Spinner
+} from "./common";
 // Import our LoginForm component to be displayed on the screen
-//import LoginForm from "./components/LoginForm";
-
 import LoginForm from "./LoginForm";
 
 class App extends Component {
-    //Life Cycle Method to init the firebase
-    componentWillMount(){
-        firebase.initializeApp({
-            apiKey: "AIzaSyDjqsPb7_HQTQktq6xl-fDGiCVBF2dPUQY",
-            authDomain: "mapp-auth-class-ahmed.firebaseapp.com",
-            databaseURL: "https://mapp-auth-class-ahmed.firebaseio.com",
-            projectId: "mapp-auth-class-ahmed",
-            storageBucket: "mapp-auth-class-ahmed.appspot.com",
-            messagingSenderId: "407294186543"
-        });
-    }
+state = { loggedIn: null };
+// Life cycle method to init the firebase
+componentWillMount() {
+firebase.initializeApp({
+apiKey: "AIzaSyDV-EREF0SeH1iSM2DYNsaCpzjZayCGWNo",
+authDomain: "rnapp-auth-class-mad.firebaseapp.com",
+databaseURL: "https://rnapp-auth-class-mad.firebaseio.com",
+projectId: "rnapp-auth-class-mad",
+storageBucket: "rnapp-auth-class-mad.appspot.com",
+messagingSenderId: "561918213506"
+});
 
+//Handle the Application when it's logged in or logged out
+firebase.auth().onAuthStateChanged(user => {
+if (user) {
+this.setState({ loggedIn: true });
+} else {
+this.setState({ loggedIn: false });
+}
+});
+}
 
+renderContent() {
+switch (this.state.loggedIn) {
+case true:
+return (
+<Card>
+<CardSection>
+<CustomButton onPress={() => firebase.auth().signOut()}>
+Logout
+</CustomButton>
+</CardSection>
+</Card>
+);
+case false:
+return <LoginForm />;
+default:
+return <Spinner size="large" />;
+}
+}
 render() {
-    return (
-        <View>
-            <Header headerText="Authentication" />
-            <LoginForm />
-        </View>
-    );
+return (
+<View>
+<Header headerText="Authentication" />
+{this.renderContent()}
+{/* 
+Before the renderContent Handling
+<LoginForm /> */}
+</View>
+);
+}
 }
 
-}
-
-export default App ;
+export default App;
